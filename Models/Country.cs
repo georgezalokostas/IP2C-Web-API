d33e98b8238fee58c7a39dacacabf54cf0db0dbf
George Zalokostas <gzalos6@gmail.com>
2023-03-27 21:08:40 +0300

    Scaffolded Tables

---
+++ Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IP2C_Web_API.Models;
+
+public partial class Country
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string TwoLetterCode { get; set; } = null!;
+
+    public string ThreeLetterCode { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<Ipaddress> Ipaddresses { get; } = new List<Ipaddress>();
+}
---
+++ Models/MasterContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IP2C_Web_API.Models;
+
+public partial class MasterContext : DbContext
+{
+    public MasterContext()
+    {
+    }
+
+    public MasterContext(DbContextOptions<MasterContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Ipaddress> Ipaddresses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-NPJCM17;Database=master;Trusted_Connection=true;TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getutcdate())");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.ThreeLetterCode)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.TwoLetterCode)
+                .HasMaxLength(2)
+                .IsUnicode(false)
+                .IsFixedLength();
+        });
+
+        modelBuilder.Entity<Ipaddress>(entity =>
+        {
+            entity.ToTable("IPAddresses");
+
+            entity.HasIndex(e => e.Ip, "IX_IPAddresses").IsUnique();
+
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getutcdate())");
+            entity.Property(e => e.Ip)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("IP");
+            entity.Property(e => e.UpdatedAt).HasDefaultValueSql("(getutcdate())");
+
+            entity.HasOne(d => d.Country).WithMany(p => p.Ipaddresses)
+                .HasForeignKey(d => d.CountryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_IPAddresses_Countries");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
