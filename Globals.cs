08287d8abf4f190ea4b91b081adc9c23241c2078
George Zalokostas <gzalos6@gmail.com>
2023-03-27 21:16:39 +0300

    Moved ConnectionString to AppSettings

---
+++ Globals.cs
@@ -0,0 +1,3 @@
+global using IP2C_Web_API.Models;
+global using System;
+global using System.Collections.Generic;
--- Models/MasterContext.cs
+++ Models/MasterContext.cs
@@ -20,8 +20,7 @@ public partial class MasterContext : DbContext
     public virtual DbSet<Ipaddress> Ipaddresses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-NPJCM17;Database=master;Trusted_Connection=true;TrustServerCertificate=true;");
+        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
