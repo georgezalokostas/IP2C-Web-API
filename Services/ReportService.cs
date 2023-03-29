88cfef4c07cd164327773d9a51bc80777b94bfcd
George Zalokostas <gzalos6@gmail.com>
2023-03-29 19:33:40 +0300

    Cached builder

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -5,6 +5,7 @@ namespace IP2C_Web_API.Services;
 public class ReportService : IReport
 {
     readonly MasterContext _context;
+    readonly IConfigurationBuilder? _builder = null;
 
     public ReportService(MasterContext context)
     {
@@ -13,16 +14,14 @@ public class ReportService : IReport
 
     public async Task<ServiceResponse<List<ReportDTO>>> GetReport(string? input)
     {
-        var serviceResponse = new ServiceResponse<List<ReportDTO>>();
-
-        var builder = new ConfigurationBuilder()
-        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-        var configuration = builder.Build();
+        var configuration = _builder?.Build() ?? new ConfigurationBuilder()
+                                                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                                                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
 
         using var con = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
         await con.OpenAsync();
+
+        var serviceResponse = new ServiceResponse<List<ReportDTO>>();
 
         if (string.IsNullOrWhiteSpace(input))
         {
