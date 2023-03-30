6fc31e259ecd2dd650e2f71b2705173ed81ca7db
George Zalokostas <gzalos6@gmail.com>
2023-03-30 22:49:42 +0300

    Refactored to a tasks class

---
+++ Helpers/Tasks.cs
@@ -0,0 +1,125 @@
+namespace IP2C_Web_API.Helpers;
+
+public class Tasks
+{
+    readonly MasterContext _context;
+    readonly Object _dictLock = new();
+    RestClient _client;
+
+    public Tasks(MasterContext context)
+    {
+        _context = context;
+        _client = new RestClient();
+    }
+ 
+    public async Task<IPDetailsDTO?> GetCachedDataAsync(string ip)
+    {
+        Console.WriteLine("GetCachedDataAsync called");
+        return await Task.Run(() =>
+        {
+            return _cachedIPs.TryGetValue(ip, out IPDetailsDTO? data) ? data : null;
+        });
+    }
+
+    public async Task<IPDetailsDTO?> GetDatabaseDataAsync(string input)
+    {
+        Console.WriteLine("GetDatabaseDataAsync called");
+        var ipAddressEntity = await _context.Ipaddresses.FirstOrDefaultAsync(x => x.Ip == input);
+        if (ipAddressEntity is null)
+            return null;
+
+        var countryEntity = await _context.Countries.FindAsync(ipAddressEntity.CountryId);
+        if (countryEntity is null)
+            return null;
+
+        return new IPDetailsDTO
+        {
+            CountryName = countryEntity.Name,
+            TwoLetterCode = countryEntity.TwoLetterCode,
+            ThreeLetterCode = countryEntity.ThreeLetterCode
+        };
+    }
+
+    public async Task<IPDetailsDTO?> GetAPIDataAsync(string ip)
+    {
+        Console.WriteLine("GetAPIDataAsync called");
+        var response = await _client.ExecuteGetAsync(new RestRequest($"https://ip2c.org/{ip}"));
+
+        if (!response.IsSuccessful)
+            return null;
+
+        var splitData = response.Content!.Split(';');
+
+        if (splitData.Length != 4) return null; //Something went wrong
+
+        return new IPDetailsDTO
+        {
+            TwoLetterCode = splitData[1],
+            ThreeLetterCode = splitData[2],
+            CountryName = splitData[3]
+        };
+    }
+
+
+    public async Task AddOrUpdateDatabaseAsync(string ip, IPDetailsDTO data)
+    {
+        Console.WriteLine("AddOrUpdateDatabaseAsync called");
+
+        var existingCountry = _context.Countries.FirstOrDefault(x => x.TwoLetterCode == data.TwoLetterCode);
+
+        // If we don't find a country, insert a new record
+        if (existingCountry == null)
+        {
+            var newCountry = new Country
+            {
+                TwoLetterCode = data.TwoLetterCode,
+                ThreeLetterCode = data.ThreeLetterCode,
+                Name = data.CountryName.Truncate(50)
+            };
+
+            _context.Countries.Add(newCountry);
+            await _context.SaveChangesAsync();
+
+            existingCountry = newCountry;
+        }
+
+        // If the IP address record already exists, update it. Else insert it
+        var existingIp = await _context.Ipaddresses.FirstOrDefaultAsync(x => x.Ip == ip);
+
+        if (existingIp is null)
+        {
+            _context.Ipaddresses.Add(new Ipaddress
+            {
+                Ip = ip,
+                CountryId = existingCountry.Id,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            });
+        }
+        else
+        {
+            existingIp.UpdatedAt = DateTime.Now;
+            _context.Ipaddresses.Update(existingIp);
+        }
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateCacheAsync(string ip, IPDetailsDTO data)
+    {
+        Console.WriteLine("UpdateCacheAsync called");
+
+        await Task.Run(() =>
+        {
+            lock (_dictLock)
+            {
+                _cachedIPs.TryAdd(ip, new IPDetailsDTO
+                {
+                    CountryName = data.CountryName,
+                    TwoLetterCode = data.TwoLetterCode,
+                    ThreeLetterCode = data.ThreeLetterCode
+                });
+            }
+        });
+    }
+}
--- Services/IPDetailsService.cs
+++ Services/IPDetailsService.cs
@@ -3,18 +3,16 @@ namespace IP2C_Web_API.Services;
 public class IPDetailsService : IIPDetails
 {
     readonly MasterContext _context;
-    readonly RestClient _client;
-    readonly Object _dictLock = new();
 
     public IPDetailsService(MasterContext context)
     {
         _context = context;
-        _client = new RestClient("https://ip2c.org/");
     }
 
     public async Task<ServiceResponse<IPDetailsDTO>> GetIPDetails(string? ip)
     {
         var serviceResponse = new ServiceResponse<IPDetailsDTO>();
+        var tasks = new Tasks(_context);
 
         if (!ValidateIP(ip))
         {
@@ -23,146 +21,36 @@ public class IPDetailsService : IIPDetails
             return serviceResponse; //Empty string or invalid IP.
         }
 
-        serviceResponse.Data = await GetCachedDataAsync(ip!);
+        serviceResponse.Data = await tasks.GetCachedDataAsync(ip!);
 
         //Found in cache.
         if (serviceResponse.Data is not null)
             return serviceResponse;
 
-        serviceResponse.Data = await GetDatabaseDataAsync(ip!);
+        serviceResponse.Data = await tasks.GetDatabaseDataAsync(ip!);
 
         //Found in database.
         if (serviceResponse.Data is not null)
         {
-            await UpdateCacheAsync(ip!, serviceResponse.Data);
+            await tasks.UpdateCacheAsync(ip!, serviceResponse.Data);
             return serviceResponse;
         }
 
-        serviceResponse.Data = await GetAPIDataAsync(ip!);
+        serviceResponse.Data = await tasks.GetAPIDataAsync(ip!);
 
         //Found in API.
         if (serviceResponse.Data is not null)
-            await Task.WhenAll(UpdateCacheAsync(ip!, serviceResponse.Data),
-                               AddOrUpdateDatabaseAsync(ip!, serviceResponse.Data));
+            await Task.WhenAll(tasks.UpdateCacheAsync(ip!, serviceResponse.Data),
+                               tasks.AddOrUpdateDatabaseAsync(ip!, serviceResponse.Data));
 
         return serviceResponse;
     }
 
     static bool ValidateIP(string? input)
     {
-        return string.IsNullOrWhiteSpace(input) ?
-                                          false :
-                                          new Regex(@"^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){3}$").IsMatch(input);
+        if (string.IsNullOrWhiteSpace(input))
+            return false;
+
+        return new Regex(@"^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){3}$").IsMatch(input);
     }
-
-    async Task<IPDetailsDTO?> GetCachedDataAsync(string ip)
-    {
-        Console.WriteLine("GetCachedDataAsync called");
-        return await Task.Run(() =>
-        {
-            return _cachedIPs.TryGetValue(ip, out IPDetailsDTO? data) ? data : null;
-        });
-    }
-
-    async Task<IPDetailsDTO?> GetDatabaseDataAsync(string input)
-    {
-        Console.WriteLine("GetDatabaseDataAsync called");
-        var ipAddressEntity = await _context.Ipaddresses.FirstOrDefaultAsync(x => x.Ip == input);
-        if (ipAddressEntity is null)
-            return null;
-
-        var countryEntity = await _context.Countries.FindAsync(ipAddressEntity.CountryId);
-        if (countryEntity is null)
-            return null;
-
-        return new IPDetailsDTO
-        {
-            CountryName = countryEntity.Name,
-            TwoLetterCode = countryEntity.TwoLetterCode,
-            ThreeLetterCode = countryEntity.ThreeLetterCode
-        };
-    }
-
-    async Task<IPDetailsDTO?> GetAPIDataAsync(string ip)
-    {
-        Console.WriteLine("GetAPIDataAsync called");
-        var response = await _client.ExecuteGetAsync(new RestRequest($"https://ip2c.org/{ip}"));
-
-        if (!response.IsSuccessful)
-            return null;
-
-        var splitData = response.Content!.Split(';');
-
-        if (splitData.Length != 4) return null; //Something went wrong
-
-        return new IPDetailsDTO
-        {
-            TwoLetterCode = splitData[1],
-            ThreeLetterCode = splitData[2],
-            CountryName = splitData[3]
-        };
-    }
-
-    async Task AddOrUpdateDatabaseAsync(string ip, IPDetailsDTO data)
-    {
-        Console.WriteLine("AddOrUpdateDatabaseAsync called");
-
-        var existingCountry = _context.Countries.FirstOrDefault(x => x.TwoLetterCode == data.TwoLetterCode);
-
-        // If we don't find a country, insert a new record
-        if (existingCountry == null)
-        {
-            var newCountry = new Country
-            {
-                TwoLetterCode = data.TwoLetterCode,
-                ThreeLetterCode = data.ThreeLetterCode,
-                Name = data.CountryName.Truncate(50)
-            };
-
-            _context.Countries.Add(newCountry);
-            await _context.SaveChangesAsync();
-
-            existingCountry = newCountry;
-        }
-
-        // If the IP address record already exists, update it. Else insert it
-        var existingIp = await _context.Ipaddresses.FirstOrDefaultAsync(x => x.Ip == ip);
-
-        if (existingIp is null)
-        {
-            _context.Ipaddresses.Add(new Ipaddress
-            {
-                Ip = ip,
-                CountryId = existingCountry.Id,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            });
-        }
-        else
-        {
-            existingIp.UpdatedAt = DateTime.Now;
-            _context.Ipaddresses.Update(existingIp);
-        }
-
-        await _context.SaveChangesAsync();
-    }
-
-    async Task UpdateCacheAsync(string ip, IPDetailsDTO data)
-    {
-        Console.WriteLine("UpdateCacheAsync called");
-
-        await Task.Run(() =>
-        {
-            lock (_dictLock)
-            {
-                _cachedIPs.TryAdd(ip, new IPDetailsDTO
-                {
-                    CountryName = data.CountryName,
-                    TwoLetterCode = data.TwoLetterCode,
-                    ThreeLetterCode = data.ThreeLetterCode
-                });
-            }
-        });
-    }
-
 }
