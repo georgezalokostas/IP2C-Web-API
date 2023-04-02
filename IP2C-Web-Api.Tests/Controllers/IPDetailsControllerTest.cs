8df01e302855cd97abb06d761dc41f6c2a5f214d
George Zalokostas <gzalos6@gmail.com>
2023-04-02 13:11:40 +0300

    Added Unit Testing, Modified Structure

---
+++ IP2C-Web-Api.Tests/Controllers/IPDetailsControllerTest.cs
@@ -0,0 +1,72 @@
+using FakeItEasy;
+using FluentAssertions;
+using IP2C_Web_API.Controllers;
+using IP2C_Web_API.Interface;
+using IP2C_Web_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IP2C_Web_Api.Tests.Controllers;
+
+public class IPDetailsControllerTest
+{
+    [Theory]
+    [InlineData("1.1.1.1")]
+    [InlineData("5.243.22.1")]
+    [InlineData("255.1.1.255")]
+    [InlineData("255.255.255.255")]
+    public async Task IPDetailsController_GetIPDetails_ReturnsOkResult(string ip)
+    {
+        // Arrange
+        var unitOfWork = A.Fake<IUnitOfWork>();
+        var serviceResponse = new ServiceResponse<IPDetailsDTO>
+        {
+            Data = new IPDetailsDTO
+            {
+                CountryName = "Test",
+                TwoLetterCode = "TS",
+                ThreeLetterCode = "TST"
+            },
+            Success = true
+        };
+
+        A.CallTo(() => unitOfWork.IPDetails.GetIPDetails(ip)).Returns(serviceResponse);
+
+        // Act
+        var response = await new IPDetailsController(unitOfWork).GetIPDetails(ip);
+
+        // Assert
+        response.Result.Should().BeOfType<OkObjectResult>()
+            .Subject.Value.Should().BeEquivalentTo(serviceResponse);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("1,2,3,4")]
+    [InlineData("1 2 3 4")]
+    [InlineData("1.1.1.256")]
+    [InlineData("Hello")]
+    [InlineData(" 5.43.23.54")]
+    [InlineData("100.0.100.100")]
+    [InlineData("50.50.50.50.")]
+    [InlineData("50.50.50.50.50")]
+    [InlineData("5.5.5.o")]
+    public async Task IPDetailsController_GetIPDetails_ReturnsNotFoundResult(string ip)
+    {
+        // Arrange
+        var unitOfWork = A.Fake<IUnitOfWork>();
+
+        A.CallTo(() => unitOfWork.IPDetails.GetIPDetails(ip)).Returns(new ServiceResponse<IPDetailsDTO>
+        {
+            Success = false,
+            Message = "Invalid IP value."
+        }
+        );
+
+        // Act
+        var response = await new IPDetailsController(unitOfWork).GetIPDetails(ip);
+
+        // Assert
+        response.Result.Should().BeOfType<NotFoundObjectResult>();
+    }
+
+}
---
+++ IP2C-Web-Api.Tests/Controllers/ReportControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using IP2C_Web_API.Controllers;
+using IP2C_Web_API.Interface;
+using IP2C_Web_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IP2C_Web_Api.Tests.Controllers
+{
+    public class ReportControllerTest
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("GR")]
+        [InlineData("GR,CN")]
+        [InlineData("GR,TEST")]
+        [InlineData("TEST,DE,CY")]
+        public async Task ReportController_GetReports_ReturnsOkResult(string? codes)
+        {
+            // Arrange
+            var unitOfWork = A.Fake<IUnitOfWork>();
+            var serviceResponse = new ServiceResponse<List<ReportDTO>>
+            {
+                Data = new List<ReportDTO>
+                {
+                    new ReportDTO
+                    {
+                        CountryName = "Test",
+                        AddressesCount = 1,
+                        LastAddressUpdated = DateTime.Now
+                    }
+                },
+                Success = true
+            };
+
+            A.CallTo(() => unitOfWork.Report.GetReport(codes)).Returns(serviceResponse);
+
+            var controller = new ReportController(unitOfWork);
+
+            // Act
+            var response = await controller.GetReports(codes);
+
+            // Assert
+            response.Result.Should().BeOfType<OkObjectResult>()
+                .Subject.Value.Should().BeEquivalentTo(serviceResponse);
+        }
+
+        [Theory]
+        [InlineData("T")]
+        [InlineData("InvalidCountryName")]
+        [InlineData("GRR")]
+        [InlineData("GEE")]
+        [InlineData("0")]
+        [InlineData("123")]
+        public async Task ReportController_GetReports_ReturnsNotFoundResult(string? codes)
+        {
+            // Arrange
+            var unitOfWork = A.Fake<IUnitOfWork>();
+            var serviceResponse = new ServiceResponse<List<ReportDTO>>
+            {
+                Success = false,
+                Message = "Invalid country codes provided. Please try again with valid codes."
+            };
+
+            A.CallTo(() => unitOfWork.Report.GetReport(codes)).Returns(serviceResponse);
+
+            var controller = new ReportController(unitOfWork);
+
+            // Act
+            var response = await controller.GetReports(codes);
+
+            // Assert
+            response.Result.Should().BeOfType<NotFoundObjectResult>();
+        }
+    }
+}
