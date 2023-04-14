26fb2698e03296e6e08a1ceff2ed218990835ac5
georgezalokostas <gzalos6@gmail.com>
2023-04-14 15:43:15 +0300

    Removed Dictionary

--- IP2C-Web-API/Globals.cs
+++ IP2C-Web-API/Globals.cs
@@ -13,10 +13,3 @@ global using System.Text.RegularExpressions;
 global using RestSharp;
 global using Microsoft.AspNetCore.Authorization;
 global using static Microsoft.Toolkit.StringExtensions;
-global using static Globals;
-
-public class Globals
-{
-    public static ConcurrentDictionary<string, IPDetailsDTO> _cachedIPs = new();
-    public static ConcurrentDictionary<string, DateTime> _cachedDateTimes = new();
-}
