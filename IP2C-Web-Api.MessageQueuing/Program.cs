3198a215d7c532cf21339c62941caceeaf87df16
George Zalokostas <gzalos6@gmail.com>
2023-04-23 12:44:45 +0300

    Added Console App to get messages

---
+++ IP2C-Web-Api.MessageQueuing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Welcome to the messaging program.");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "password",
+    VirtualHost = "/"
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("queue", durable: true, exclusive:false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"A message has been received: {message}");
+};
+
+channel.BasicConsume("queue", true, consumer);
+
+Console.ReadKey();
